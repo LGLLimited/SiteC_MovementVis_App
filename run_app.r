c715c674f74eee5996e5022ef211bc5b293f3c3c
Ben Cox <bcox@blueleaf.local>
2022-08-09 14:48:59 -0700

    uncomment required pkgs

--- run_app.r
+++ run_app.r
@@ -3,9 +3,9 @@ library(lubridate)
 library(sf)
 library(shiny)
 library(leaflet)
-#library(shinyWidgets)
-#library(htmlwidgets)
-#library(leaflegend)
+library(shinyWidgets)
+library(htmlwidgets)
+library(leaflegend)
 #library(reactlog)
 
 # Get the underlying data
