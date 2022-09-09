b89e544e4e553dae4379af7546e066831c25e4b0
Ben Cox <bcox@blueleaf.local>
2022-09-09 14:31:08 -0700

    Info page updates, pic size adjustments

--- ui_code.r
+++ ui_code.r
@@ -2,14 +2,15 @@
 ui <- fluidPage(titlePanel(title=img(src="BC-Hydro-Logo.png",height="56px",width="100px",style="float: right;"),
                            windowTitle="Site C Fish Movement"),
                 tabsetPanel(id="main",
-                            tabPanel("Data description",
+                            tabPanel("Info",
                                      fluidPage(
                                        titlePanel(h1("Site C Fish Movement Assessment",align='center')),
-                                     fluidRow(column(width=4, data_description()),
-                                     column(width=8,img(src="Project.png",width="50%",style="float: right; padding-right: 200px",title="Site C Project"))),
+                                       br(),br(),br(),
+                                     fluidRow(column(width=5, data_description(),style="text-align: justify;font-size: 16px"),
+                                     column(width=7,img(src="Project.png",width="75%",style="float: left; padding-left: 200px",title="Site C Project"))),
                                      br(),
-                                     fluidRow(column(width=4),
-                                     column(width=8,img(src="Antenna.png",title="A fixed-station antenna on the Peace River.", width="50%",style="float: right; padding-right: 200px"))))),
+                                     fluidRow(column(width=5),
+                                     column(width=7,img(src="Antenna.png",title="A fixed-station antenna on the Peace River.", width="75%",style="float: left; padding-left: 200px"))))),
                             # TEXT OUTPUT FOR WELCOME PAGE
                             tabPanel("Map",
                                      titlePanel(h1("Fixed Receiver Locations in the Peace River Basin",align='center')),
@@ -21,18 +22,22 @@ ui <- fluidPage(titlePanel(title=img(src="BC-Hydro-Logo.png",height="56px",width
                                        sidebarPanel(width=3,
                                         # make hr() black
                                        tags$head(tags$style(HTML("hr {border-top: 1px solid #000000;}"))),
+                                       # play button size
+                                       tags$head(tags$style(type='text/css', ".slider-animate-button { font-size: 20pt !important;}")),
+                                       # shift play button to left and pad away from slider
+                                       tags$head(tags$style(type='text/css', ".slider-animate-container { text-align: left; margin-top: -2px}")),
                                        # remove minor ticks on animation slider
                                        tags$style(type = "text/css", ".irs-grid-pol.small {height: 0px;}"),
                                        h1("Plot settings"),
                                        hr(),
-                                       radioButtons("basemap", "Choose basemap.", choices = c("Terrain", "Satellite"), selected = "Terrain"),
+                                       radioButtons("basemap", "Select basemap.", choices = c("Terrain", "Satellite"), selected = "Terrain"),
                                        actionButton("center_map", "Re-center Map"),
                                        br(),
                                        br(),
                                        uiOutput("choiceUI"),
                                        uiOutput("lifestageUI"),
                                        br(),
-                                       sliderTextInput("anim_speed","Choose animation speed.",choices = c("Slowest","Slower","Faster","Fastest"), selected ="Faster",grid = TRUE),
+                                       sliderTextInput("anim_speed","Select animation speed.",choices = c("Slowest","Slower","Faster","Fastest"), selected ="Faster",grid = TRUE),
                                        br(),
                                        uiOutput("dateUI"),
                                        ),
@@ -40,7 +45,7 @@ ui <- fluidPage(titlePanel(title=img(src="BC-Hydro-Logo.png",height="56px",width
       tags$head(tags$style(HTML('.irs-from, .irs-to, .irs-min, .irs-max{visibility: hidden !important;}'))),
       tabsetPanel(id="tabs",
         tabPanel("Individual Movements",
-                mainPanel(leafletOutput("map1",height="85vh",width = "85vh"))),
+                mainPanel(leafletOutput("map1", width = "85vh", height = "85vh"))),
         tabPanel("Seasonal Distribution",
                 mainPanel(leafletOutput("map2",height="85vh",width = "85vh")))
                 )
